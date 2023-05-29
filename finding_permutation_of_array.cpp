1b6c1b71345c4092beee178642a2244808581ce8
Syed Mehdi Kazim <64413768+syedmehdikazim@users.noreply.github.com>
2023-05-29 13:38:21 +0530

    Create finding_permutation_of_array.cpp
    the code will do the following (we can see the permutations of the input, which has three elements is 3! which is 6)
    
    Input: nums = [1,2,3]
    Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]

---
+++ finding_permutation_of_array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+
+    void permutation(vector<int> nums,int index, vector<vector<int>>&ans)
+    {
+        if(index>=nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for(int j=index; j<nums.size(); j++)
+        {
+            swap(nums[j],nums[index]);
+            permutation(nums,index+1,ans);
+            swap(nums[j],nums[index]);
+        }
+       
+    }
+
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        int index=0;
+        permutation(nums,index,ans);
+        return ans;
+    }
+};
