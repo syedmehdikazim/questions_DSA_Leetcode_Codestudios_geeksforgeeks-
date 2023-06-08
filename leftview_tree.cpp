d169859203654deb7cb2c949e977b25edfd05bca
Syed Mehdi Kazim <64413768+syedmehdikazim@users.noreply.github.com>
2023-06-08 15:28:14 +0530

    Create leftview_tree.cpp

---
+++ leftview_tree.cpp
@@ -0,0 +1,12 @@
+void solve(Node* root, vector<int> &ans, int level) {
+    //base case
+    if(root == NULL)
+        return ;
+       
+    //we entered into a new level    
+    if(level == ans.size())
+        ans.push_back(root->data);
+        
+    solve(root->left, ans, level+1);
+    solve(root->right, ans, level+1);
+}
