d385baf2c9abcf3ec5216c220959b493a0edc6e0
Syed Mehdi Kazim <64413768+syedmehdikazim@users.noreply.github.com>
2023-06-13 16:00:55 +0530

    Create rightview_binarytree.cpp

---
+++ rightview_binarytree.cpp
@@ -0,0 +1,21 @@
+void solve(Node* root, vector<int> &ans, int level) {
+    //base case
+    if(root == NULL)
+        return ;
+       
+    //we entered into a new level    
+    if(level == ans.size())
+        ans.push_back(root->data);
+    
+  //just by interchanging the two statment we can have right view
+    solve(root->right, ans, level+1);
+    solve(root->left, ans, level+1);
+    
+}
+
+vector<int> leftView(Node *root)
+{
+   vector<int> ans;
+   solve(root, ans, 0);
+   return ans;
+}
