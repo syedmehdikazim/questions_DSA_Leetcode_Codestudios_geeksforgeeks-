5e8069e3f84a425dcd3975c7c4f587b1530e6d7c
Syed Mehdi Kazim <64413768+syedmehdikazim@users.noreply.github.com>
2023-06-28 18:00:35 +0530

    Create kth_smallestelement_using_priorityqueue.cpp

---
+++ kth_smallestelement_using_priorityqueue.cpp
@@ -0,0 +1,33 @@
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        
+        priority_queue<int>pq;
+        
+        //step1
+        
+        for(int i=0;i<k;i++)
+        {
+            pq.push(arr[i]);
+        }
+        
+        //step 2
+        
+        for(int i=k; i<=r; i++)
+        {
+            if(pq.top()>arr[i])
+            {
+                pq.pop();
+                pq.push(arr[i]);
+            }
+        }
+        
+        int ans=pq.top();
+        return ans;
+        
+    }
+};
